a5d4cc27a7ea029f96ccfa8ca9c04a8ca4425f9f
VrushabhRepo <vrushabhchimankar10@gmail.com>
2024-09-06 11:36:06 +0530

    Second commit.

---
+++ src/Sept_06092024/Lab002.java
@@ -0,0 +1,13 @@
+
+//This is the package
+package Sept_06092024;
+//This is ths class name
+public class Lab002 {
+
+    //This is the main method where execution starts
+    public static void main(String[] args) {
+
+        //This statement is used to print the statement
+        System.out.println("hello Vrushabh");
+    }
+}
