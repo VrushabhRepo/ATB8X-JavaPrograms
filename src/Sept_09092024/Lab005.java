296c925bc6e3e068dcba965a6c6be8c2c4e5938a
VrushabhRepo <vrushabhchimankar10@gmail.com>
2024-09-09 14:46:56 +0530

    Second commit.

---
+++ src/Sept_09092024/Lab005.java
@@ -0,0 +1,17 @@
+package Sept_09092024;
+
+public class Lab005 {
+
+    public static void main(String[] args) {
+
+        //Keyword & identifiers
+
+      //boolean, true, false, public, class, static, void, main --> keywords
+       //Always starts with lowercase
+       //Variable is used to store value
+       boolean is_vrushabh_single = true;
+       int a = 10;
+
+
+    }
+}
